f535ff65c8facf91198c1e48f3e4e0126eeccf8a
ToniMigliato <tonimigliato@gmail.com>
2024-08-04 09:49:05 -0300

    adds `.nvmrc` and `.gitignore` files

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <div>
+        <h1>TabNews 6</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
