cdd9e80c7f7edbff7fd4a31c8a4e591f810e0cfd
ToniMigliato <tonimigliato@gmail.com>
2024-08-04 10:48:29 -0300

    adds `.env` and `database.js` files

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
+import database from "../../../../../infra/database.js";
+
 export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log("DATABASE: ", result.rows[0]);
   return response.status(200).json({ message: "API working Ok!" });
 }
